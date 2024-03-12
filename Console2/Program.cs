fd7be9d029f252563a2061fa4684b25de96f15e3
Usuario <Usuario@CPD02-M20>
2024-03-12 11:54:51 -0300

    Adicionar arquivos de projeto.

---
+++ Console2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe a nota do aluno:");
+            double nota = double.Parse(Console.ReadLine());
+            Console.WriteLine("informe a frequencia do aluno")
+                int f = int.Parse(Console.ReadLine());
+            if (nota >=7 || f >= 75)
+            {
+                Console.WriteLine()
+            }
+
+        }
+    }
+}
